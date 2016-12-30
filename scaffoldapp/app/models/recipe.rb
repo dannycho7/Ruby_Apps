00662ec702df701156adbaa5bf8790c389d65283
Danny H. Cho <dannycho7@gmail.com>
2016-12-29 23:25:47 -0800

    bug

--- scaffoldapp/app/models/recipe.rb
+++
@@ -1,4 +0,0 @@
-class Recipe < ApplicationRecord
-	validates_length_of :title, :within => 1..20
-	validates_uniqueness_of :title, :message => "already exists"
-end
--- scaffoldapp/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-	root 'recipes#index'
-  resources :recipes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
