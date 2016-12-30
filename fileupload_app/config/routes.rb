334c58bd22ce12e567c9eefcbb791454f46a12ef
Danny H. Cho <dannycho7@gmail.com>
2016-12-29 23:51:19 -0800

    File Upload bugged

---
+++ fileupload_app/config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+root "resumes#index"
+  get 'resumes/index'
+
+  get 'resumes/new'
+
+  get 'resumes/create'
+
+  get 'resumes/destroy'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
